694eae56af7c20219c8375728c7ce26a4207a4c6
KaajalShah-3101 <kajshahcommonapp@gmail.com>
2025-04-14 18:07:15 -0700

    Final changes

--- rockpaperscissorsreal/ContentView.swift
+++ rockpaperscissorsreal/ContentView.swift
@@ -8,103 +8,136 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    let moves = ["rock", "paper", "scissors"]
+    
+  
+   
+    
+    @State private var playerHand = "rock"
+    @State private var cpuHand = "paper"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
+    
     var body: some View {
-        
-        
-        ZStack{
+        ZStack {
             Color(.systemMint)
                 .ignoresSafeArea()
             
-            VStack {
-                Spacer()
+            VStack(spacing: -30) {
                 
+                
+                // Logo Image (centered)
                 Image("logo")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                    .scaleEffect(2.0)
-                    
-                    
+                    .frame(maxWidth: 300)
+                    .padding()
                 
-                Spacer()
-                    
                 
-                HStack {
-                    
-                   Spacer()
-                    
-                    Image("rock")
+                
+                // Rock & Paper (evenly spaced)
+                HStack(spacing: -100) {
+                    Image(playerHand)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .padding(.all)
-                        .scaleEffect(3.0)
-                        
+                        .frame(width: 300, height: 300)
                     
-                    
-                   Spacer()
-                    
-                    Image("paper")
+                    Image(cpuHand)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                        .padding(.all)
-                        .scaleEffect(3.0)
-                    
-                    Spacer()
-                    
-                       
+                        .frame(width: 300, height: 300)
+                }
+                .padding()
+                
+                Button {
+                    makeYourMove()
+                    determineWinner()
+                } label: {
+                    Image("button")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: 300)
+                        .padding()
                 }
                 
-                Spacer()
-               
-                Image("button")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .padding(.all)
-                    .scaleEffect(1.5)
-                    
+                
+                // Button Image (centered)
+                
                 
                 Spacer()
                 
+                // Scoreboard (centered, evenly spaced)
                 HStack {
-                    
                     Spacer()
                     
                     VStack {
-                        Text("Player")
-                        Text("0")
+                        Text("Player 1")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
                     }
                     
                     Spacer()
                     
                     VStack {
-                        Text("Player")
-                        Text("0")
+                        Text("Player 2")
+                            .font(.headline)
+                            .padding(.bottom,10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
                     }
                     
                     Spacer()
-                    
-                    
                 }
-                
-                Spacer()
                 
                 
             }
-            
-            
-            
         }
         
         
         
-        
-        
-        
-        
-        
-        
     }
+    
+    func makeYourMove() {
+            // Randomly select player move and computer move safely
+            if let randomPlayerMove = moves.randomElement(),
+               let randomComputerMove = moves.randomElement() {
+                playerHand = randomPlayerMove
+               cpuHand = randomComputerMove
+            }
+        }
+        
+        // Function to determine the winner and update scores
+        func determineWinner() {
+            if playerHand == cpuHand {
+                // It's a tie, no score changes
+                return
+            }
+            
+            switch (playerHand, cpuHand) {
+            case ("rock", "scissors"), ("scissors", "paper"), ("paper", "rock"):
+                // Player wins
+                playerScore += 1
+            case ("scissors", "rock"), ("paper", "scissors"), ("rock", "paper"):
+                // Computer wins
+                cpuScore += 1
+            default:
+                break
+            }
+        }
+    
+    
+    
+    
+    
 }
+
+
 
 #Preview {
     ContentView()
